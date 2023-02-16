f7520c9fb1409846f265672b011b6bf34cb00ef9
kich_alex654@mail.ru <kich_alex654@mail.ru>
2023-02-16 13:18:33 +0300

    Добавил AgGridReact

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
-import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 function App() {
   const [keyword, setKeyword] = useState('');
   const [data, setData] = useState([]);
   const fetchData = () => {
     // REST API call
-    fetch('https://api.github.com/search/repositories?q='+keyword)
+    fetch('https://api.github.com/search/repositories?q=' + keyword)
       .then(response => response.json())
       .then(data => setData(data.items))
       .catch(err => console.error(err))
   }
+  const columns = [
+    {field: 'full_name', sortable: true, filter: true},
+    {field: 'html_url', sortable: true, filter: true},
+    {field: 'owner.login', sortable: true, filter: true},
+    {
+      field: 'full_name',
+      cellRenderer: params =>
+      <button
+      onClick={() => alert(params.value)}>
+      Press me
+      </button>
+      }
+    ]
 
   return (
     <div className="App">
@@ -18,21 +33,15 @@ function App() {
         value={keyword}
         onChange={e => setKeyword(e.target.value)} />
       <button onClick={fetchData}>Fetch</button>
-      <table>
-        <tbody>
-          {
-            data.map(repo =>
-              <tr>
-                <td>{repo.full_name}</td>
-                <td>
-                  <a href={repo.html_url}>
-                    {repo.html_url}</a>
-                </td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
+      <div className="ag-theme-material"
+        style={{ height: 500, width: '90%' }}>
+        <AgGridReact
+          rowData={data}
+          columnDefs={columns}
+          pagination={true}
+          paginationPageSize={8}
+        />
+      </div>
     </div>
   );
 }
